7b15892ce4c24597af2e0400847130e61214b6f5
Udit Bhatia <ubbhatia@amazon.com>
2019-06-05 15:28:57 -0700

    saving keras model

--- src/tf/keras_1/train.py
+++ src/tf/keras_1/train.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-
+import argparse
+import os
 
 def train(x_train, y_train):
 
@@ -21,7 +22,7 @@ def predict(model, x_predict):
 
 
 
-def main():
+def main(model_dir):
 
     xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
     ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
@@ -30,6 +31,15 @@ def main():
           y_train=ys)
     print("Prediction: {}".format(model.predict([10.0])))
 
+    model.save("{}/{}".format(model_dir,"model.h5"))
 
-if __name__ == "__main__":
-    main()
+
+if __name__ =='__main__':
+
+    parser = argparse.ArgumentParser()
+
+    # input data and model directories
+    parser.add_argument('--model_dir', type=str)
+
+    args, _ = parser.parse_known_args()
+    main(model_dir=args.model_dir)
