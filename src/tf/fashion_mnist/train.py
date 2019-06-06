7d95bf8b33be88361e17a7a05c04590e57ef2f30
Udit Bhatia <ubbhatia@amazon.com>
2019-06-06 13:10:56 -0700

    Adding train script for dfashion mnist

--- src/tf/fashion_mnist/train.py
+++ src/tf/fashion_mnist/train.py
@@ -1,4 +1,5 @@
 from tensorflow import keras
+import tensorflow as tf
 
 def load_data():
     print("Loading Fashion Mnist dataset")
@@ -7,11 +8,30 @@ def load_data():
     print(
         "Fashion Mnist dataset loaded with, train_images: {}, train_labels: {}, test_images: {}, test_labels: {}".format(
             train_images.shape, train_labels.shape, test_images.shape, test_labels.shape))
+    return (train_images, train_labels), (test_images, test_labels)
+
+def build():
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=tf.train.AdamOptimizer,
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+    return model
 
 def main():
-    load_data()
+    (train_images, train_labels), (test_images, test_labels) = load_data()
 
     print("Starting training fashion mnist")
+    model = build()
+    model.fit(train_images,train_labels, epochs=5)
+
+    print("Evaluate model:")
+    model.evaluate(test_images,test_labels)
+    
 
 if __name__ == "__main__":
     main()
